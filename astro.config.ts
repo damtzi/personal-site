882bdf93fb08f0e84212b6301652bc1abb549129
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-04-04 19:31:41 +0200

    chore: Biome check

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
