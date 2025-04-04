c97f247223e135ce55f1895ece449396181eb59d
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2025-04-04 12:38:55 +0200

    chore: Convert astro config file to a ts file

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://damitzi.com',
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    integrations: [react(), mdx()]
+});
